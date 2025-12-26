7f011d3ae5aeaecf11a87a41d01387ed62601d99
nitesh-0 <niteshsah760@gmail.com>
2025-12-26 16:52:21 +0545

    speed insight feature added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoute";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
